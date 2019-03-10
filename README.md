51846c4bd68cb5107f0e92196554c37cb4d5d51a
Kasin <kasinsparks@gmail.com>
2019-03-09 18:04:59 -0600

    A batch script for the installation of Minecraft Forge Client, Mods, and Server Assets

---
+++ README.md
@@ -0,0 +1,6 @@
+1. Clone or Download
+2. Run installer.bat
+3. LAUNCH MINECRAFT then proceed to LAUNCH OPTIONS and click ADD NEW
+4. Scroll down and SELECT 1.7.10, then LAUNCH MINECRAFT { this is necessary for Minecraft to download that version's assets }
+5. EXIT MINECRAFT then REPEAT STEPS 4 & 5 FOR 1.7.10 FORGE
+6. ENABLE JVM ARGUEMENTS then COPY & PASTE from JVM ARGS file provided
---
+++ installer.bat
@@ -0,0 +1,93 @@
+@rem author: Kasin Sparks
+@rem collaborator: Jerin Sparks
+@rem version: 1.0
+@rem date: 9 March 19
+
+@echo off
+color 2
+
+echo --------------------------------------------------------------------------------
+echo                                     WARNING!
+echo                 Your mods and config folder will be OVERWRITTEN!                          
+echo --------------------------------------------------------------------------------
+
+:loop
+set /p opt=Do you wish to continue? (Y/N):
+
+if "%opt%" == "y" (
+	set opt=Y
+) else if "%opt%" == "n" (
+	set opt=N
+)
+
+
+if "%opt%" == "Y" (
+	goto continue
+) else if "%opt%" == "N" (
+	exit
+)
+goto loop
+
+:continue
+
+echo --------------------------------------------------------------------------------
+echo                     DarkLight Matrix Executable Initialized.
+echo --------------------------------------------------------------------------------
+
+if exist C:\\Users\%username%\AppData\Roaming\.minecraft (
+	echo What is real? 
+	echo How do you define 'real'? 
+	echo If you're talking about what you can feel...
+	echo what you can smell...
+	echo what you can taste and see...
+	echo then 'real' is simply electrical signals interpreted by your brain. -Morpheus
+	if exist C:\\Users\%username%\AppData\Roaming\.minecraft\config (
+		rmdir /s /q C:\\Users\%username%\AppData\Roaming\.minecraft\config
+	)
+	
+	if exist C:\\Users\%username%\AppData\Roaming\.minecraft\mods (
+		rmdir /s /q C:\\Users\%username%\AppData\Roaming\.minecraft\mods
+	)
+
+	WinRAR.exe x -ibck %CD%\minecraft.rar . C:\\Users\%username%\AppData\Roaming\.minecraft\
+	WinRAR.exe x -ibck %CD%\mods.rar . C:\\Users\%username%\AppData\Roaming\.minecraft\
+	WinRAR.exe x -ibck %CD%\config.rar . C:\\Users\%username%\AppData\Roaming\.minecraft\
+	
+	@rem minecraft forge server will detect if files were not unpacked when started
+) else (
+	echo                        You must of taken the blue pill...
+	echo                            Please install Minecraft.
+	echo --------------------------------------------------------------------------------
+	pause
+	exit
+)
+
+if exist C:\\Users\%username%\AppData\Roaming\.minecraft\versions\1.7.10-Forge10.13.4.1614-1.7.10 (
+	echo --------------------------------------------------------------------------------
+	echo                        Forge has already been installed!
+) else (
+	echo --------------------------------------------------------------------------------
+	echo                                We got trouble...
+	echo --------------------------------------------------------------------------------
+	forge-1.7.10-10.13.4.1614-1.7.10-installer-win.exe
+	pause
+	echo --------------------------------------------------------------------------------
+	if exist C:\\Users\%username%\AppData\Roaming\.minecraft\versions\1.7.10-Forge10.13.4.1614-1.7.10 (
+		echo                            Forge has been installed!
+		del /q forge-1.7.10-10.13.4.1614-1.7.10-installer-win.exe.log
+	) else (
+		echo                            Forge can't be installed!
+		echo                         Contact Aotyrek For Assistance.
+		echo --------------------------------------------------------------------------------
+		pause
+		exit
+	)
+)
+echo --------------------------------------------------------------------------------
+echo I'm trying to free your mind, %username%...
+echo But I can only show you the door...
+echo You're the one that has to walk through it -Morpheus
+echo --------------------------------------------------------------------------------
+echo                         Welcome to the Matrix %username%
+echo --------------------------------------------------------------------------------
+pause
